435801bd16313e3b1df796d02ae73c4d88f85d15
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2026-04-14 16:41:40 +0530

    mysql migration and auto db setup

--- QuantityMeasurementAPI/Program.cs
+++ QuantityMeasurementAPI/Program.cs
@@ -79,8 +79,13 @@ builder.Services.AddCors(options =>
 // =====================
 // DATABASE (SQL SERVER)
 // =====================
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")!));
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    ));
 
 // =====================
 // DEPENDENCY INJECTION
@@ -117,6 +122,11 @@ builder.Services.AddAuthentication(options =>
 });
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 // =====================
 // RAILWAY PORT SUPPORT
