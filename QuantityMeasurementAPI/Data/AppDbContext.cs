8f62e8549e8f7e09d7f635a8477c1568493730d1
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2026-04-14 22:49:13 +0530

    fix missing AppDbContext mappings

--- QuantityMeasurementAPI/Data/AppDbContext.cs
+++ QuantityMeasurementAPI/Data/AppDbContext.cs
@@ -1,7 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using ModelLayer.Entity;
-using ModelLayer.Enum;
-using RepositoryLayer.Interface;
 
+namespace RepositoryLayer.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
 
+        public DbSet<MeasurementRecord> Measurements { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<MeasurementRecord>().ToTable("measurements");
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<ApplicationUser>().ToTable("ApplicationUsers");
+        }
+    }
+}
