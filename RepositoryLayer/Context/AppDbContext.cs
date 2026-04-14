530a9d5672fb02b61140fc0eb57cedd924e155e2
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2026-04-14 22:58:59 +0530

    fix postgres lowercase table mapping

--- RepositoryLayer/Context/AppDbContext.cs
+++ RepositoryLayer/Context/AppDbContext.cs
@@ -18,34 +18,40 @@ namespace RepositoryLayer.Context
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+{
+    base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<MeasurementRecord>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.FromUnit).HasMaxLength(50);
-                entity.Property(e => e.ToUnit).HasMaxLength(50);
-                entity.Property(e => e.MeasurementType).HasConversion<string>();
-                entity.Property(e => e.OperationType).HasConversion<string>();
-                entity.Property(e => e.ErrorMessage).HasMaxLength(500);
-            });
+    modelBuilder.Entity<MeasurementRecord>(entity =>
+    {
+        entity.ToTable("measurements");
 
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.HasIndex(e => e.Email).IsUnique();
-                entity.Property(e => e.Email).HasMaxLength(256).IsRequired();
-                entity.Property(e => e.PasswordHash).IsRequired();
-            });
+        entity.HasKey(e => e.Id);
+        entity.Property(e => e.FromUnit).HasMaxLength(50);
+        entity.Property(e => e.ToUnit).HasMaxLength(50);
+        entity.Property(e => e.MeasurementType).HasConversion<string>();
+        entity.Property(e => e.OperationType).HasConversion<string>();
+        entity.Property(e => e.ErrorMessage).HasMaxLength(500);
+    });
 
-            modelBuilder.Entity<ApplicationUser>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.HasIndex(e => e.Email).IsUnique();
-                entity.Property(e => e.Email).HasMaxLength(256).IsRequired();
-            });
-        }
+    modelBuilder.Entity<User>(entity =>
+    {
+        entity.ToTable("users");
 
+        entity.HasKey(e => e.Id);
+        entity.HasIndex(e => e.Email).IsUnique();
+        entity.Property(e => e.Email).HasMaxLength(256).IsRequired();
+        entity.Property(e => e.PasswordHash).IsRequired();
+    });
+
+    modelBuilder.Entity<ApplicationUser>(entity =>
+    {
+        entity.ToTable("applicationusers");
+
+        entity.HasKey(e => e.Id);
+        entity.HasIndex(e => e.Email).IsUnique();
+        entity.Property(e => e.Email).HasMaxLength(256).IsRequired();
+    });
+}
 
         public new int SaveChanges()
         {
