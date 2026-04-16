c79275cf73054b4c90b115c60ac7cf8fd7a86e62
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2026-04-16 10:23:25 +0530

    fix user entity table mapping

--- ModelLayer/Entity/User.cs
+++ ModelLayer/Entity/User.cs
@@ -1,13 +1,18 @@
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ModelLayer.Entity
 {
+    [Table("Users")]
     public class User
     {
         public int Id { get; set; }
+
         public string Email { get; set; } = string.Empty;
+
         public string PasswordHash { get; set; } = string.Empty;
+
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
-
